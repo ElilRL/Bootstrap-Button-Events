a2cf7289989f9d44f16669542b8c0ea9c7ff27fa
Eliel <125eramirez@frhsd.com>
2024-01-25 09:48:42 -0500

    done

---
+++ js/KeyboardEvent.js
@@ -0,0 +1,24 @@
+//Row 2
+//Keyboard Events
+
+
+//Key Down
+let inputOne= document.getElementById('inputOne');
+inputOne.addEventListener('keydown', function(e){
+    inputOne.value = e.code;
+    e.preventDefault()
+    console.log(e.code);
+});
+//key up
+let inputTwo= document.getElementById('inputTwo');
+inputTwo.addEventListener('keyup', function(e){
+    if(inputTwo )
+})
+
+
+let inputThree= document.getElementById('inputThree');
+
+
+let inputFour= document.getElementById('inputFour');
+
+
--- js/clickEvent.js
+++ js/clickEvent.js
@@ -4,7 +4,13 @@ btnOne.addEventListener('click', function(event){
 });
 
 
-let btnTwo = document.getElementById('dblclick');
-btnTwo.addEventListener('dblclick', function(event){
- event.target.style.border = "4px dotted #ec8f6a";
+let btnThree = document.getElementById('rightClick');
+btnThree.addEventListener('contextmenu' , function(e){
+    e.target.style.background = "#a0b96b";
+});
+
+
+let btnFour = document.getElementById('mousePointer');
+btnFour.addEventListener('mouseenter' , function(ev){
+    ev.target.style.background = "#69fffc";
 });
